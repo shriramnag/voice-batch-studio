58e3c1dedd7017f1166efb72cf65b5384696866e
Shriram Nag <Shriramjdp494.0.0@gmail.com>
2026-01-01 22:45:10 +0530

    Create t3_config.py

---
+++ src/voicebatch_studio/models/t3/modules/t3_config.py
@@ -0,0 +1,44 @@
+# VoiceBatch Studio Pro - T3 Configuration
+from ..llama_configs import LLAMA_CONFIGS
+
+class T3Config:
+    def __init__(self, text_tokens_dict_size=704):
+        # टेक्स्ट सेटिंग्स
+        self.start_text_token = 255
+        self.stop_text_token = 0
+        self.text_tokens_dict_size = text_tokens_dict_size
+        self.max_text_tokens = 2048
+
+        # स्पीच (आवाज़) सेटिंग्स
+        self.start_speech_token = 6561
+        self.stop_speech_token = 6562
+        self.speech_tokens_dict_size = 8194
+        self.max_speech_tokens = 4096
+
+        # मॉडल सेटिंग्स
+        self.llama_config_name = "Llama_520M"
+        self.input_pos_emb = "learned"
+        self.speech_cond_prompt_len = 150
+
+        self.encoder_type = "voice_encoder"
+        self.speaker_embed_size = 256
+        self.use_perceiver_resampler = True
+        self.emotion_adv = True
+
+    @property
+    def n_channels(self):
+        return LLAMA_CONFIGS[self.llama_config_name]["hidden_size"]
+    
+    @property
+    def is_multilingual(self):
+        return self.text_tokens_dict_size == 2454
+
+    @classmethod
+    def english_only(cls):
+        """सिर्फ इंग्लिश मॉडल के लिए"""
+        return cls(text_tokens_dict_size=704)
+    
+    @classmethod 
+    def multilingual(cls):
+        """मल्टी-लैंग्वेज (हिंदी आदि) मॉडल के लिए"""
+        return cls(text_tokens_dict_size=2454)
