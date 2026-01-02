b0ad50da697df33a50b208bf00b17cb431de49b1
Shriram Nag <Shriramjdp494.0.0@gmail.com>
2026-01-02 23:12:28 +0530

    Update class_utils.py

--- src/voicebatch_studio/utils/class_utils.py
+++ src/voicebatch_studio/utils/class_utils.py
@@ -1,7 +1,5 @@
-# class_utils.py
 import torch
 
-# यह डिक्शनरी एरर को दूर करने के लिए ज़रूरी है
 COSYVOICE_EMB_CLASSES = {
     'speech_tokenizer': 'voicebatch_studio.models.s3tokenizer.s3tokenizer.S3Gen',
 }
